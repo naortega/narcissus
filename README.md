516fc50e5222cfa21847022bb85a519337477dfd
Nicolás Ortega Froysa <nicolas@ortegas.org>
2024-05-01 10:21:28 +0200

    Initial commit.
    Signed-off-by: Nicolás Ortega Froysa <nicolas@ortegas.org>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Narcissus
+
+A quick script for mirroring my repositories to GitHub.
+
+## License & Copyright
+
+Since this is a meaningless little script, I'm using [the Unlicense](/LICENSE).
---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while read line
+do
+	ORIG=$(echo $line | cut -d ';' -f1)
+	DEST=$(echo $line | cut -d ';' -f2)
+	NAME=${ORIG##*/}
+
+	echo "Mirroring $NAME"
+
+	if [ -d "$NAME" ]
+	then
+		rm -r $NAME
+	fi
+
+	git clone --mirror $ORIG
+	cd $NAME
+	git push --mirror $DEST
+	cd ..
+	rm -r $NAME
+
+	"Done."
+done <./repo-list
